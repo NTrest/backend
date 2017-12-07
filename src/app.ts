1e87a510f5f43e10b8bbd0f2f9f5a3c710ccc4b5
Ben Brecher <bb16n@my.fsu.edu>
2017-12-07 03:09:14 -0500

    CORS

--- src/app.ts
+++ src/app.ts
@@ -62,8 +62,8 @@ port = util.normalizePort(process.env.PORT || port);
 app.set('port', port);
 
 
-/*app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "localhost");
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "ntrestapp.me");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
   });//*/
